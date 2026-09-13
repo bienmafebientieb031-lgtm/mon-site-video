ca9a232b89e871712ccd75e9d9436d9591e0ddaa
bienmafebientieb031-lgtm <bienmafebientieb031@gmail.com>
2026-09-13 14:03:13 +0200

    Update script.js
    deuxieme changement

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const videos = [
     description: "Remplace cette vidéo par les tiennes."
   },
   {
-    videoId: "TkbNbL_0HCI&t=62s",
+    videoId: "TkbNbL_0HCI&t",
     title: "Deuxième exemple",
     description: "Tu peux ajouter autant de vidéos que tu veux."
   }
